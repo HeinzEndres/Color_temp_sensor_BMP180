1a364d535862a0ad62cdeb68bd11fa118bac8206
HeinzEndres <heinz.endres@thws.de>
2024-12-09 15:03:01 +0100

    Some comments

--- Color_temp_sensor_BMP180.ino
+++ Color_temp_sensor_BMP180.ino
@@ -3,6 +3,29 @@
 #include <U8g2lib.h>
 #include "Adafruit_NeoPixel.h"
 
+//
+// Author: Heinz Endres
+//
+// Description:
+//   Small application to display temperature and pressure values from BMP180,
+//   and show a color between blue and red for temperatures between 20° and 30° celsius
+//   using the onboard LED.
+// 
+//   The code is based on BMP180_C3-Mini_0.66-OLED_cloudfree.ino by Mwala Timothy / Carenuity
+//
+// History:
+//   v1.0    HE     14-Oct-24     First implementation
+//   v1.1    HE      9-Dec-24     Some detailed comments
+//
+// Board used:
+//   Lolin C3 Mini from "esp32" Espressif Systems boards v 2.0.9.
+//
+// Libraries required:
+//   Adafruit_BMP085_Library v1.2.4
+//   U8g2 v 2.33.15
+//   Adafruit_NeoPixel v1.12.3
+//
+
 // LED info
 # define pinNumber 7
 # define numLEDs 1
@@ -26,6 +49,8 @@ void setup() {
     while (1) {}
   } 
 
+  Serial.println("Starting BMP180 measurement:");
+
   // LED setup
   LEDRing.setBrightness(200);
   // NeoPixel Bibliothek initialisieren
@@ -37,10 +62,10 @@ void setup() {
   u8g2.begin();
   
   // Set a cool font (choose from options below)
-   u8g2.setFont(u8g2_font_ncenB08_tr);  // Nice serif font, size 8
-  // u8g2.setFont(u8g2_font_fur20_tn);   // Futuristic, size 20 (works for bigger text)
-  // u8g2.setFont(u8g2_font_helvR10_tf);  // Helvetica-like font, size 10 (similar to Arial)
-  //u8g2.setFont(u8g2_font_t0_12_tr);    // Pixel font, size 12 (looks retro)
+  u8g2.setFont(u8g2_font_ncenB08_tr);  // Nice serif font, size 8
+  //u8g2.setFont(u8g2_font_fur20_tn);   // Futuristic, size 20 (works for bigger text)
+  //u8g2.setFont(u8g2_font_helvR10_tf); // Helvetica-like font, size 10 (similar to Arial)
+  //u8g2.setFont(u8g2_font_t0_12_tr);   // Pixel font, size 12 (looks retro)
 }
 
 void loop() {
@@ -48,15 +73,20 @@ void loop() {
   u8g2.clearBuffer();
   
   // Display temperature
-  u8g2.setCursor(31, 25);  // Set cursor position
-  u8g2.print("Temperature:");
-  u8g2.setCursor(35, 38);  // Set cursor position
+  u8g2.setCursor(33, 25);  // Set cursor position
+  u8g2.print("Temp:");
+  u8g2.setCursor(33, 38);  // Set cursor position
   float temp = bmp.readTemperature();
   u8g2.print(temp);
-  u8g2.print("C");
+  u8g2.print(" C");
+
+  // Output temperature to serial monitor
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.println(" °C");
   
   // Display pressure
-  u8g2.setCursor(32, 48);
+  u8g2.setCursor(33, 48);
   u8g2.print("Pressure:");
   
   // Convert pressure to a string
@@ -67,7 +97,8 @@ void loop() {
   u8g2.print(pressureStr);
   u8g2.print(" hPa");
 
-  // Output pressure to Serial Monitor
+  // Output pressure to serial monitor
+  Serial.print("Pressure:    ");
   Serial.print(pressureStr);
   Serial.println(" hPa");
 
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Small application to display temperature and pressure values from BMP180,
 and show a color between blue and red for temperatures between 20° and 30° celsius
 using the onboard LED.
+
+The code is based on BMP180_C3-Mini_0.66-OLED_cloudfree.ino by Mwala Timothy / Carenuity
